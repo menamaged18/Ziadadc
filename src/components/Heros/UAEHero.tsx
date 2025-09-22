84c7d6bef0c71e08aad60cb79cbca0d0fa64eeb9
mina maged <menamaged669@gmail.com>
2025-09-22 22:36:04 +0300

    solving responsive issues

--- src/components/Heros/UAEHero.tsx
+++ src/components/Heros/UAEHero.tsx
@@ -11,31 +11,31 @@ const UAEHero: React.FC<IHero> = ({ setShowUaeHero, showUaeHero }) => {
       style={{ backgroundImage: `url(/UAE.jpg)` }}
     >
       <div className="absolute inset-0 bg-black/50"></div>
-      <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="relative md:max-w-7xl max-w-6xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center max-w-4xl mx-auto">
-          <h2 className="text-4xl md:text-5xl font-bold text-white mb-6 leading-tight">
+          <h2 className="text-3xl md:text-4xl font-bold text-white md:mb-6 mb-3 leading-tight">
             Want to set up a company in 
             <span className="block mt-2 text-red-700 hover:text-red-800">UAE</span>
           </h2>
-          <p className="text-xl text-gray-200 mb-8 max-w-2xl mx-auto leading-relaxed">
+          <p className="md:text-xl text-lg text-gray-200 mb-8 max-w-2xl mx-auto leading-relaxed">
             Join thousands of successful businesses who&apos;ve accelerated their growth with our proven solutions. 
             Ready to unlock your potential?
           </p>
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <button 
-            className="text-white font-semibold py-4 px-8 rounded-lg transition-all duration-300 transform hover:scale-105 shadow-lg cursor-pointer bg-[#c21d25] border border-[#c21d25] hover:bg-[#a01820] hover:border-[#a01820]"
-          >
-            Get Started Today
-          </button>
-          <button 
-            className="font-semibold py-4 px-8 rounded-lg transition-all transform hover:scale-105 duration-300 bg-[#1b6a3d] text-white hover:bg-[#14502e] cursor-pointer"
-          >
-            Learn More
-          </button>
+            <button 
+              className="text-white font-semibold md:py-4 md:px-8 py-2 px-4 rounded-lg transition-all duration-300 transform hover:scale-105 shadow-lg cursor-pointer bg-[#c21d25] border border-[#c21d25] hover:bg-[#a01820] hover:border-[#a01820]"
+            >
+              Get Started Today
+            </button>
+            <button 
+              className="font-semibold md:py-4 md:px-8 py-2 px-4 rounded-lg transition-all transform hover:scale-105 duration-300 bg-[#1b6a3d] text-white hover:bg-[#14502e] cursor-pointer"
+            >
+              Learn More
+            </button>
           </div>
         </div>
       </div>
-      <p className='text-white text-4xl font-bold absolute top-130 left-1/2 -translate-x-1/2 -translate-y-1/2'>or</p>
+      <p className='text-white text-2xl md:text-4xl my-2 font-bold absolute top-130 left-1/2 -translate-x-1/2 -translate-y-1/2'>or</p>
       <HeroToggle setShowUaeHero={setShowUaeHero} showUaeHero={showUaeHero} />
     </section>
   );
